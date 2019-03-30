ac56d72148e017ba73f90e801f9001557aeb1a2c
Code Hz <CodeHz@outlook.com>
2019-03-30 16:32:25 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM codehz/archlinux as builder
+
+WORKDIR /data
+ARG CORE_VERSION="1.10.0.7"
+RUN pacman -Syu --needed --noconfirm wget unzip tcc
+RUN wget https://minecraft.azureedge.net/bin-linux/bedrock-server-$CORE_VERSION.zip -O /tmp/bedrock.zip
+RUN unzip /tmp/bedrock.zip -d ./server
+ADD launcher.c /tmp/launcher.c
+RUN tcc -o ./server/launcher /tmp/launcher.c
+RUN /packager.sh ./server/bedrock_server . && rm -rf data && mkdir data && echo '[]' > ./server/ops.json
+
+FROM scratch
+
+COPY --from=builder /data /
+WORKDIR /server
+CMD ["/server/launcher"]
---
+++ launcher.c
@@ -0,0 +1,60 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <unistd.h>
+
+#define mask 0766
+
+void do_copy(char *src, char *dst) {
+    printf("copying %s to %s\n", src, dst);
+    int input, output;
+    assert((input = open(src, O_RDONLY)) + 1);
+    assert((output = creat(dst, mask)) + 1);
+    off_t bytesCopied = 0;
+    struct stat fileinfo = {0};
+    fstat(input, &fileinfo);
+    assert(sendfile(output, input, &bytesCopied, fileinfo.st_size) + 1);
+    close(input);
+    close(output);
+}
+
+void do_link(char *src, char *dst) {
+    assert(unlink(dst) + 1);
+    assert(symlink(src, dst) + 1);
+}
+
+void do_link_dir(char *src, char *dst) {
+    // assert(rmdir(dst) + 1);
+    assert(symlink(src, dst) + 1);
+}
+
+void do_prepare(char *name, char *dataname) {
+    if (access(dataname, F_OK) == -1) {
+        do_copy(name, dataname);
+    }
+    do_link(dataname, name);
+}
+
+void do_prepare_dir(char *name, char *dataname) {
+    if (access(dataname, F_OK) == -1) {
+        mkdir(dataname, mask);
+    }
+    do_link_dir(dataname, name);
+}
+
+#define prepare(name) do_prepare("/server/" name, "/data/" name)
+#define prepare_dir(name) do_prepare_dir("/server/" name, "/data/" name)
+
+int main() {
+    prepare("ops.json");
+    prepare("permissions.json");
+    prepare("whitelist.json");
+    prepare("server.properties");
+    prepare_dir("worlds");
+    execl("./bedrock_server", NULL);
+}
