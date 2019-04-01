6c43734d3e7ee1e7535a48d2aa21c730523732d1
Code Hz <CodeHz@outlook.com>
2019-04-01 18:19:41 +0800

    fix gcc failed

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -exuo pipefail
 pacman -Syu --needed --noconfirm wget unzip gcc
 wget https://minecraft.azureedge.net/bin-linux/bedrock-server-$CORE_VERSION.zip -O /tmp/bedrock.zip
 unzip /tmp/bedrock.zip -d ./server
-gcc -o -Os ./server/launcher /tmp/launcher.c -leditline -lpthread
+gcc -Os -o ./server/launcher /tmp/launcher.c -leditline -lpthread
 strip ./server/launcher
 /packager.sh ./server/bedrock_server .
 /packager.sh ./server/launcher .
